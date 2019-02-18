f6a12e20f5fb6bfca45720ce3560ddad725c8aea
Louis Hervé <louisdousherve@gmail.com>
2019-02-18 12:28:31 +0100

    Updated virtualenv handling

---
+++ fetch_dependencies.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/zsh
+
+python3.7 -m virtualenv venv && source venv/bin/activate && pip install -r requirements.txt
---
+++ save_dependencies.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/zsh
+
+source venv/bin/activate && pip freeze > requirements.txt
